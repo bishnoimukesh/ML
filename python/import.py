8b9d88c1c1502dd36a15101b3ed296ba63fd4c2d
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2019-02-05 17:27:33 +0530

    Create import.py

---
+++ python/import.py
@@ -0,0 +1,25 @@
+import time
+
+for i in  "Prashant Tiwari":
+	print(i)
+	time.sleep(1)
+
+#######################################
+#another way of importing
+"""
+from time import sleep
+
+for i in "Prashant Tiwari":
+	print(i)
+	sleep(1)
+"""
+
+#######################################
+#yet another way of importing
+"""
+from time import sleep as delay
+
+for i in "Prashant Tiwari":
+	print(i)
+	delay(1)
+"""
