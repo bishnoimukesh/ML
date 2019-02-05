246925bae21c6ceb05c6f8f180bbd665d57e7b5c
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2019-02-05 17:23:39 +0530

    Create first.py

---
+++ python/first.py
@@ -0,0 +1,34 @@
+
+print("Welcome")
+
+
+a = input("Enter a :")
+b = input("Enter b :")
+
+c = a + b
+print (c)
+
+#to convert a and b to integer, we would cast them in int with int()
+a = int(a)
+b = int(b)
+c = a + b
+print(c)
+
+
+#python gives you freedoom to use operators with as much
+#potenial it can provide
+#you can use + to add twwo strings,
+#you can use * to multiply string to an integer
+a = input("Enter another a : ")
+b = input("Enter another b : ")
+print(a + b)
+print(a * 5)
+print((a + " ") * 5)
+
+#this is single line comment
+
+"""
+this is mulitple
+line
+commeent
+"""
