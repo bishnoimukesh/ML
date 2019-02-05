55ad4956ef6175449949257d2c5aeab3b9e44943
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2019-02-05 17:24:47 +0530

    Create if.py

---
+++ python/if.py
@@ -0,0 +1,29 @@
+
+print("Hello World")
+
+a = input("Enter a : ")
+b = input("Enter b : ")
+c = int(a) + int(b)
+
+print ("Sum is : " + str(c) )
+
+if(c > 100):
+	print("Greater than 100")
+
+print("End of program, this statement will always print itself, whatver if result may fall to")
+
+"""
+if(c > 100):
+
+	print( "Greater than 100" )
+
+	if(c > 200):
+		print( "Greater than 200" )
+	print( "Hello" )
+
+elif(c > 50):
+	print( "Greater than 50" )
+else:
+	print( "Smaller than 50" )
+print( "End of Program" )
+"""
